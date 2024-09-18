0e61e246ee381015ad228e7f75454727c466d46c
Yura <urijkisil2007@gmail.com>
2024-09-18 22:13:16 +0300

    first master commit

---
+++ Lab_03_4.cpp/Lab_03_4.cpp/Lab_03_4.cpp.cpp
@@ -0,0 +1,25 @@
+﻿// Lab_03_4.cpp
+// < Кісіль Юрій >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 7
+#include <iostream>
+using namespace std;
+int main()
+{
+	double R; // вхідний аргумент
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+	
+	// розгалуження в повній формі
+	if ((x >= -2 * R && x <= 0 && y >=0 && y <= 2 * R && R > 0 && pow((x + R), 2) + pow((y - R), 2) <= pow(R, 2)) ||
+		(y <= 0 && y >= -R && x >= 0 && x <= 2 * R && R > 0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
---
+++ АП ЛР-03.2/АП ЛР-03.2/АП ЛР-03.2.cpp
@@ -0,0 +1,44 @@
+﻿// Lab_03_2.cpp
+// < Кісіль Юрій >
+// Лабораторна робота № 3.2
+// Розгалуження, задане формулою: функція з параметрами.
+// Варіант 7
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x; // вхідний аргумент
+    double a; // вхідний параметр
+    double b; // вхідний параметр
+    double c; // вхідний параметр
+    double F; // результат обчислення виразу
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+    cout << "x = "; cin >> x;
+
+
+    if (x < 5 && c != 0)
+        F = -a * pow(x, 2) - b;
+    else
+        if (x > 5 && c == 0)
+            F = (x - a) / x;
+        else
+            F = -x / c;
+
+ 
+    /*
+    if (x < 5 && c != 0)
+        F = -a * pow(x, 2) - b;
+    if (x > 5 && c == 0)
+        F = (x - a) / x;
+    if (!(x < 5 && c != 0) && !(x > 5 && c == 0))
+        F = -x / c;
+    */
+
+    cout << " F = " << F << endl;
+    cin.get();
+    
+    return 0;
+}
